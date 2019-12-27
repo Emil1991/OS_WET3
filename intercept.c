39b9fc5fcee6fa007622eecac9e68f4a4c0c2aa6
Emil1991 <emilgulayev@gmail.com>
2019-12-27 14:17:46 +0000

    order call funtion fix

--- intercept.c
+++ intercept.c
@@ -17,25 +17,8 @@ asmlinkage long our_sys_kill(int pid, int sig){
 //TODO: complete the function
 kill(pid,sig);
 }
-
 /*
-This function updates the entry of the kill system call in the system call table to point to our_syscall. 
-*/
-void plug_our_syscall(void){
-// TODO: complete the function
-    sys_call_table[37] = (unsigned long*)our_sys_kill;
-}
-
-/*
-This function updates the entry of the kill system call in the system call table to point to the original kill system call. 
-*/
-void unplug_our_syscall(void){
-// TODO: complete the function
-    sys_call_table[37] = (void*)original_syscall;
-}
-
-/*
-turns on the R/W flag for addr. 
+turns on the R/W flag for addr.
 */
 void allow_rw(unsigned long addr){
     write_cr0(read_cr0() & (~addr));
@@ -52,18 +35,37 @@ void disallow_rw(unsigned long addr) {
 
 
 /*
+This function updates the entry of the kill system call in the system call table to point to our_syscall. 
+*/
+void plug_our_syscall(void){
+// TODO: complete the function
+    allow_rw(sys_call_table[37]);
+    original_syscall= (void*)sys_call_table[37];
+    sys_call_table[37] = (unsigned long*)our_sys_kill;
+}
+
+/*
+This function updates the entry of the kill system call in the system call table to point to the original kill system call. 
+*/
+void unplug_our_syscall(void){
+// TODO: complete the function
+    sys_call_table[37] = (void*)original_syscall;
+    disallow_rw(sys_call_table[37]);
+}
+
+
+
+/*
 This function is called when loading the module (i.e insmod <module_name>)
 */
 int init_module(void) {
    // TODO: complete the function
     sys_call_table = (unsigned long**)kallsyms_lookup_name("sys_call_table");
-    allow_rw(sys_call_table[37]);
-    original_syscall= (void*)sys_call_table[37];
     plug_our_syscall();
 }
 
 void cleanup_module(void) {
    // TODO: complete the function
    unplug_our_syscall();
-   disallow_rw(sys_call_table[37]);
+
 }
