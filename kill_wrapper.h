45920c03738de481b5a4edd940f6863061f8689e
Emil1991 <emilgulayev@gmail.com>
2019-12-27 13:19:01 +0000

    WET3 origress

--- kill_wrapper.h
+++ kill_wrapper.h
@@ -1,8 +1,30 @@
 //
 // Created by student on 12/20/19.
 //
+#include <linux/kernel.h>
+#include <stdio.h>
+
+
 
 #ifndef WET3_KILL_WRAPPER_H
 #define WET3_KILL_WRAPPER_H
+
+int kill(pid_t pid) {
+    int __res;
+    printf("welcome to our kill wrapper!");
+
+    __asm__(
+    "syscall;"
+    : "=a" (__res)
+    : "0" (37), "D" (pid)
+    : "memory");
+
+    if ((__res) < 0) {
+//        errno = (-__res);
+        return -1;
+    }
+    return __res;
+}
+
 
 #endif //WET3_KILL_WRAPPER_H
